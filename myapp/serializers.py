f58ffcee77fcb49e8958d6e20fe0907b3ae7a685
Mrishab337 <84860544+Mrishab337@users.noreply.github.com>
2024-12-04 19:51:55 +0530

    Update serializers.py

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -6,3 +6,10 @@ class BusinessregisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Businessregister
         fields = '__all__'
+        
+    def validated_workspacename(self, value):
+        if Businessregister.objects.filter(workspacename=value).exists():
+            raise serializers.ValidationError("already exists")
+        return value
+    def create(self, validated_data):
+        return Businessregister.objects.create(**validated_data)
