eef3a5183067911d9ee720f6e3742b0cc6ca4e90
Mrishab337 <84860544+Mrishab337@users.noreply.github.com>
2024-12-04 19:51:13 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-# Create your models here.
-
 class Businessregister(models.Model):
     business_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=100)
-    workspacename=models.CharField(max_length=100)
-    
+    workspacename = models.CharField(max_length=100, unique=True)
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True, unique=True)
+
+    def __str__(self):
+        return self.business_name
