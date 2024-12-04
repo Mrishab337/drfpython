3a0b2041bf9f785c010c7696c0a4c4105c5e5757
Mrishab337 <84860544+Mrishab337@users.noreply.github.com>
2024-12-04 19:54:48 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -2,9 +2,30 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Businessregister
 from .serializers import BusinessregisterSerializer
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
 
 # Create your views here.
 
 class BusinessregisterViewSet(viewsets.ModelViewSet):
     queryset = Businessregister.objects.all()
     serializer_class = BusinessregisterSerializer
+    
+    
+    
+    ###- for name suggetions [ not used ] ##
+    
+# @api_view(['GET'])
+# def namesuggetions(request):
+#     suggetions=[]
+#     business = Businessregister.objects.all()
+#     existednames=[business.workspacename for business in business]
+#     basename=request.query_params.get('basename','workspace')
+#     for i in range(1,6):
+#         suggetions=f"{basename}_{i}"
+#         if suggetions not in existednames:
+#             suggetions.append(suggetions)
+#         return Response(suggetions)
+    
+    
